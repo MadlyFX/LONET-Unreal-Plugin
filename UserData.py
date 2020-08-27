66f699f9c28bb4985427861e7f1389e1a2308b61
Andy Jarosz <andy@andyfilms.net>
2020-08-26 22:10:33 -0500

    1.3.0

--- UserData.py
+++ UserData.py
@@ -4,7 +4,7 @@ import sys
 UDP_IP = "192.168.0.162"
 UDP_PORT = 3333
 PREFIX = bytes.fromhex("F2")
-MESSAGE = ",TEST,1,2,3,4,5,6"
+MESSAGE = ",Hello,5555,2,3,4,5,6"
 BUFFER = PREFIX + bytes(MESSAGE, "utf-8")
 
 print("UDP target IP: %s" % UDP_IP)
@@ -13,4 +13,9 @@ print("message: %s" % MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+sock.sendto(BUFFER, (UDP_IP, UDP_PORT))
+
+MESSAGE = ",World,7,8,9,10,11,12"
+BUFFER = PREFIX + bytes(MESSAGE, "utf-8")
+
 sock.sendto(BUFFER, (UDP_IP, UDP_PORT))
